02871a586696ff45da6670002c206ed15283f83c
Diogo Henrique <sistemas5@springhoteles.com>
2024-09-05 12:47:24 +0100

    Snippets and Settings

---
+++ Recommended/settings.js
@@ -0,0 +1,97 @@
+// ======================================================== \\
+const settings =
+{
+  "workbench.productIconTheme": "fluent-icons",
+  "workbench.iconTheme": "symbols",
+  "workbench.colorTheme": "Default Dark+",
+  "errorLens.excludeBySource": [
+    "eslint(no-useless-escape)",
+    "eslint(no-undef)"
+  ],
+  "[vue]": {
+    "editor.defaultFormatter": "Vue.volar"
+  },
+  "symbols.hidesExplorerArrows": false,
+  "editor.tabSize": 2,
+  "editor.indentSize": "tabSize",
+  "better-comments.highlightPlainText": true,
+  "editor.fontLigatures": false,
+  "eslint.format.enable": true,
+  "eslint.alwaysShowStatus": true,
+  "eslint.codeAction.showDocumentation": {
+    "enable": true
+  },
+  "javascript.format.enable": true,
+  "typescript.suggest.paths": false,
+  "javascript.suggest.paths": false,
+  "peacock.favoriteColors": [
+    {
+      "name": "Angular Red (Pastel)",
+      "value": "#F4A1A4"
+    },
+    {
+      "name": "Azure Blue (Pastel)",
+      "value": "#7FBFFF"
+    },
+    {
+      "name": "JavaScript Yellow (Pastel)",
+      "value": "#FDF6B2"
+    },
+    {
+      "name": "Mandalorian Blue (Pastel)",
+      "value": "#8BAAD6"
+    },
+    {
+      "name": "Node Green (Pastel)",
+      "value": "#8FAF90"
+    },
+    {
+      "name": "React Blue (Pastel)",
+      "value": "#AEE4FB"
+    },
+    {
+      "name": "Something Different (Pastel)",
+      "value": "#C597B0"
+    },
+    {
+      "name": "Svelte Orange (Pastel)",
+      "value": "#FFA17F"
+    },
+    {
+      "name": "Vue Green (Pastel)",
+      "value": "#9ED1B6"
+    },
+    {
+      "name": "Dark Marine",
+      "value": "#020926"
+    },
+    {
+      "name": "Marine",
+      "value": "#04124d"
+    },
+    {
+      "name": "Purple",
+      "value": "#3c044d"
+    },
+    {
+      "name": "Lime",
+      "value": "#1c850c"
+    },
+    {
+      "name": "Lava",
+      "value": "#870909"
+    },
+    {
+      "name": "Sky",
+      "value": "#098787"
+    },
+    {
+      "name": "Pedro",
+      "value": "#692f08"
+    }
+  ],
+  "peacock.affectActivityBar": false,
+  "peacock.affectStatusBar": false,
+  "peacock.surpriseMeOnStartup": true,
+  "peacock.surpriseMeFromFavoritesOnly": true
+}
